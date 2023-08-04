deefa43fc9cbc8e36170889e59e824b2e24d0108
Sandy2004 <134693152+sandeepa-oraon@users.noreply.github.com>
2023-08-04 16:06:54 +0530

    Add files via upload

---
+++ asset/page/main/app.js
@@ -0,0 +1,18 @@
+function main(){
+    
+    location.href='../main/main.html';
+
+
+    
+
+}
+
+function search(){
+    
+
+    location.href= '../search/index.html';
+}
+
+function india_best(){
+    location.href='../songs/index.html';
+}
